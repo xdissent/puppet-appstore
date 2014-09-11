2b2306ae8da8e526df5ef81bf4cfe5d120598709
Tim Sharpe <tim@sharpe.id.au>
2014-09-11 16:17:51 +1000

    WIP

--- lib/puppet/parser/functions/appstore_password.rb
+++ lib/puppet/parser/functions/appstore_password.rb
@@ -1,13 +1,13 @@
-module Puppet::Parser::Function
+module Puppet::Parser::Functions
   newfunction(:appstore_password, :type => :rvalue) do |args|
     hiera_password = lookupvar('appstore::password')
-    appstore_user = lookupvap('appstore::appleid')
+    appstore_user = lookupvar('appstore::appleid')
 
     command = "security -q find-generic-password -w -a #{appstore_user} -s 'Apple App Store' /Users/#{lookupvar('::boxen_user')}/Library/Keychains/login.keychain"
 
-    if hiera_password == :undefined || hiera_password.nil?
+    if [:undefined, :undef, nil].include?(hiera_password)
       begin
-        Puppet::Util::Execution.execute(command).to_str
+        Puppet::Util::Execution.execute(command).to_str.strip
       rescue Puppet::ExecutionFailure => detail
         raise Puppet::ParseError, "Failed to lookup App Store password from keychain"
       end
